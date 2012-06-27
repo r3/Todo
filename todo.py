6f7128e8312822554103587f9e3afb7f6ac8d798
r3 <ryan.roler@gmail.com>
2012-06-26 22:51:42 -0700

    Added shortcuts for subprocessors. Added todo.edit and the ability for users to edit their reminders with $EDITOR. Fixed pretty printer to just print the single line reminder if there's only one. Set argparse to convert my serial number input to ints for me. No more of those issues! Added warning for issues between Python 2 & 3 versions of pickling. I'd like to resolve that in the future.

--- todo.py
+++ todo.py
@@ -9,6 +9,10 @@
     Trivial Todo is a command line driven program used to track reminders. It
     is capable of handling due dates, catagories for your reminders, and more.
     Use `todo --help` for a complete list of options.
+
+    A note on compatibility between 2 and 3: databases created in Python 3 are
+    not backwards compatible as Python 3's pickle uses another protocol. Pick
+    a version and stick with it, or create a new database.
 """
 
 import sys
@@ -173,23 +177,26 @@ def _parse_relative_date(date):
 
 def _print_results(results):
     """Helper function used to display results"""
-    catagories = {}
-    try:
-        for result in results:
-            catagories.setdefault(result.catagory, []).append(result)
-
-        for catagory in catagories:
-            print("{}:".format(catagory))
-            for reminder in catagories[catagory]:
-                string = "\t#{serial} - {content}"
-
-                if reminder.date_due:
-                    string += " ({date_due})"
-
-                print(string.format(**reminder.__dict__))
-
-    except TypeError:
+    if isinstance(results, Reminder):
         print(results)
+        return
+    elif len(results) == 1:
+        print(results[0])
+        return
+
+    catagories = {}
+    for result in results:
+        catagories.setdefault(result.catagory, []).append(result)
+
+    for catagory in catagories:
+        print("{}:".format(catagory))
+        for reminder in catagories[catagory]:
+            string = "\t#{serial} - {content}"
+
+            if reminder.date_due:
+                string += " ({date_due})"
+
+            print(string.format(**reminder.__dict__))
 
 
 def _create_new_database(path):
@@ -301,7 +308,7 @@ def add(args):
 
 def remove(args):
     """Called by the 'remove' subparser"""
-    reminder = search_field(int(args.serial), 'serial')[0]
+    reminder = search_field(args.serial, 'serial')[0]
     if not args.confirm:
         print("Remove '{}'?".format(reminder))
 
@@ -336,13 +343,29 @@ def show(args):
     """Called by the 'show' subparser"""
     if args.serial:
         try:
-            return _print_results(search_field(int(args.serial), 'serial')[0])
+            return _print_results(search_field(args.serial, 'serial')[0])
         except ValueError:
             raise InvalidSerialException("{} is not a valid serial number")
     elif args.catagory:
         return _print_results(search_field(args.catagory, 'catagory'))
     else:
         return _print_results(list(_iter_reminders()))
+
+
+def edit(args):
+    content = tempfile.mktemp()
+    reminder = search_field(args.serial, 'serial')[0]
+    _remove_reminder(reminder)
+
+    with open(content, 'w') as text:
+        text.write(reminder.content)
+
+    subprocess.call([os.getenv('EDITOR'), content])
+
+    with open(content) as text:
+        reminder.content = '\n'.join(text.readlines()).strip()
+
+    _append_reminder(reminder)
 
 
 if __name__ == '__main__':
@@ -363,7 +386,8 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(help="Commands for todo:")
 
     # Add reminders
-    parser_add = subparsers.add_parser('add', help="add reminders")
+    parser_add = subparsers.add_parser('add', help="add reminders",
+            aliases=('a',))
     parser_add.add_argument('content', help="""text for your reminder. If
             omitted, your $EDITOR will be launched to produce the reminder
             content""", nargs='?', default=None)
@@ -375,16 +399,17 @@ if __name__ == '__main__':
 
     # Remove reminders
     parser_remove = subparsers.add_parser('remove', help="""remove reminders
-            by number""")
+            by number""", aliases=('rm',))
     parser_remove.add_argument('serial', help="""number of the reminder to be
-            removed""", metavar='number')
+            removed""", metavar='NUMBER', type=int)
     parser_remove.add_argument('--yes', '-y', action='store_const',
             const=True, help="""bypasses request for approval before removing
             the reminder""", dest='confirm', default=None)
     parser_remove.set_defaults(func=remove)
 
     # Search reminders
-    parser_search = subparsers.add_parser('search', help="search reminders")
+    parser_search = subparsers.add_parser('search', help="search reminders",
+            aliases=('sh',))
     parser_search.add_argument('content', help="find reminders by content",
             nargs='?', default=None)
     parser_search.add_argument('--due', '-d', help="find reminders by due date",
@@ -392,13 +417,21 @@ if __name__ == '__main__':
     parser_search.set_defaults(func=search)
 
     # Show reminders
-    parser_show = subparsers.add_parser('show', help="show reminders")
+    parser_show = subparsers.add_parser('show', help="show reminders",
+            aliases=('sw',))
     group = parser_show.add_mutually_exclusive_group()
     group.add_argument('--number', '-n', help="show a reminder by its number",
-            dest='serial', default=None, metavar='NUMBER')
+            dest='serial', default=None, metavar='NUMBER', type=int)
     group.add_argument('--catagory', '-c', help="""show reminders in a
             catagory""", default=None)
     parser_show.set_defaults(func=show)
+
+    # Edit reminder
+    parser_edit = subparsers.add_parser('edit', help="edit a reminder",
+            aliases=('e',))
+    parser_edit.add_argument('serial', help="""number of the reminder to be
+            edited""", metavar='NUMBER', type=int)
+    parser_edit.set_defaults(func=edit)
 
     args = parser.parse_args()
 
